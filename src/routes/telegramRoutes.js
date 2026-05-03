004bf99bf8758a48442484d254746466336f54fc
Yogesh Kaushik <Yogeshkaushik138@gmail.com>
2026-05-03 22:37:20 +0530

    Fix Telegram account routes

--- src/routes/telegramRoutes.js
+++ src/routes/telegramRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use(protect);
 
 router.post('/connect', telegramController.connectAccount);
-router.get('/', telegramController.getAccounts);
-router.delete('/:id', telegramController.disconnectAccount);
+router.get('/accounts', telegramController.getAccounts);
+router.delete('/accounts/:id', telegramController.disconnectAccount);
 
 module.exports = router;
