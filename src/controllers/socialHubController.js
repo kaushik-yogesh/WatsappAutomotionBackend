aba15b21998b5ec819360ecccabeab489dea0467
Yogesh Kaushik <Yogeshkaushik138@gmail.com>
2026-07-20 23:33:48 +0530

    Add r_member_social scope for LinkedIn API to fix permission error

--- src/controllers/socialHubController.js
+++ src/controllers/socialHubController.js
@@ -782,7 +782,7 @@ exports.getInsights = async (req, res, next) => {
 exports.getLinkedInAuthUrl = (req, res) => {
   const clientId = process.env.LINKEDIN_CLIENT_ID;
   const redirectUri = process.env.LINKEDIN_REDIRECT_URI;
-  const scope = ['openid', 'profile', 'w_member_social'].join(' ');
+  const scope = ['openid', 'profile', 'w_member_social', 'r_member_social'].join(' ');
 
   const url = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
 
